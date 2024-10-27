7fbbc4a507c5746459cff62fb7622f9c032573a3
Nik Gilmore <nik@nikgilmore.com>
2024-10-26 19:52:16 -0700

    Add audio file support to extras/image_upload MimeType

--- src/extras/image_upload.rs
+++ src/extras/image_upload.rs
@@ -101,7 +101,7 @@ pub async fn load_image(filename: PathString) -> Response {
     };
     Response::ok().with_body(bytes).with_header(
         "content-type",
-        ImageMimeType::try_from_filename(filename)
+        MimeType::try_from_filename(filename)
             .map(|mt| mt.to_string())
             .unwrap_or("application/octet-stream".to_string()),
     )
@@ -130,64 +130,125 @@ pub fn update_image_md() -> impl IntoResponse {
 pub struct Image {
     pub metadata: ImageMetadata,
     #[allow(unused)]
-    pub mime_type: ImageMimeType,
+    pub mime_type: MimeType,
     pub bytes: Vec<u8>,
 }
 
 #[derive(Clone)]
-pub enum ImageMimeType {
+pub enum MimeType {
+    // Image File Types
     Jpeg,
     Gif,
     Png,
     Webp,
+    // Audio File Types
+    Wave,
+    Mp3,
+    Ogg,
+    Aac,
+    Webm,
+    Midi,
+    // Others go here, add as needed.
+    Unknown,
 }
-impl Display for ImageMimeType {
+impl Display for MimeType {
     fn fmt(
         &self,
         f: &mut std::fmt::Formatter<'_>,
     ) -> std::fmt::Result {
-        f.write_str(match self {
-            ImageMimeType::Jpeg => "image/jpeg",
-            ImageMimeType::Gif => "image/gif",
-            ImageMimeType::Png => "image/png",
-            ImageMimeType::Webp => "image/webp",
-        })
+        let data = match self {
+            // Image Types
+            MimeType::Jpeg => "image/jpeg",
+            MimeType::Gif => "image/gif",
+            MimeType::Png => "image/png",
+            MimeType::Webp => "image/webp",
+            // Audio Types
+            MimeType::Wave => "audio/wave",
+            MimeType::Mp3 => "audio/mpeg",
+            MimeType::Ogg => "audio/ogg",
+            MimeType::Aac => "audio/aac",
+            MimeType::Webm => "audio/webm",
+            MimeType::Midi => "audio/midi",
+            // Everything Else
+            MimeType::Unknown => "application/octect-stream",
+        };
+        f.write_str(data)
     }
 }
 
-impl ImageMimeType {
+// TODO: Merge this MIME type with the logic in the main application logic.
+impl MimeType {
+    fn is_image(&self) -> bool {
+        match self {
+            MimeType::Jpeg | MimeType::Gif | MimeType::Png | MimeType::Webp => true,
+            _ => false,
+        }
+    }
+    fn is_audio(&self) -> bool {
+        match self {
+            MimeType::Wave
+            | MimeType::Mp3
+            | MimeType::Ogg
+            | MimeType::Aac
+            | MimeType::Webm
+            | MimeType::Midi => true,
+            _ => false,
+        }
+    }
+}
+
+impl MimeType {
     pub fn try_from_filename(filename: &str) -> Result<Self, crate::Error> {
         let ext = filename
             .split('.')
             .last()
             .ok_or(crate::Error::BadRequest("Invalid filename provided.".into()))?;
         let mime_type = match ext {
+            // Image
             "jpg" | "jpeg" => Self::Jpeg,
             "gif" => Self::Gif,
             "png" => Self::Png,
             "webp" => Self::Webp,
+            // Audio
+            "aac" => Self::Aac,
+            "mp3" => Self::Mp3,
+            "wav" | "wave" => Self::Wave,
+            "weba" => Self::Webm,
+            "mid" | "midi" => Self::Midi,
+            "ogg" | "oga" | "opus" => Self::Ogg,
             _ => Err(crate::Error::BadRequest("Invalid file format requested".into()))?,
         };
         Ok(mime_type)
     }
 }
-impl FromStr for ImageMimeType {
+impl FromStr for MimeType {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_lowercase().as_str() {
+            // Image
             "image/jpeg" => Ok(Self::Jpeg),
             "image/gif" => Ok(Self::Gif),
             "image/png" => Ok(Self::Png),
             "image/webp" => Ok(Self::Webp),
-            _ => Err(format!("Not a valid ImageMimeType: {}", s)),
+            // Audio
+            "audio/wave" => Ok(Self::Wave),
+            "audio/mpeg" => Ok(Self::Mp3),
+            "audio/ogg" => Ok(Self::Ogg),
+            "audio/aac" => Ok(Self::Aac),
+            "audio/webm" => Ok(Self::Webm),
+            "audio/midi" => Ok(Self::Midi),
+            // Everything Else
+            "application/octect-stream" => Ok(Self::Unknown),
+            _ => Ok(Self::Unknown),
         }
     }
 }
 
 #[allow(unused)]
-trait GetFileDetails {
-    fn get_image_mime_type(&self) -> Option<ImageMimeType>;
+pub trait GetFileDetails {
+    fn get_image_mime_type(&self) -> Option<MimeType>;
+    fn get_audio_mime_type(&self) -> Option<MimeType>;
     fn get_content_type(&self) -> Option<&str>;
     fn get_filename(&self) -> Option<String>;
 }
@@ -196,12 +257,18 @@ impl GetFileDetails for MultipartPart {
     fn get_content_type(&self) -> Option<&str> {
         self.headers.get("content-type").map(|s| s.as_str())
     }
-    fn get_image_mime_type(&self) -> Option<ImageMimeType> {
+    fn get_image_mime_type(&self) -> Option<MimeType> {
         self.headers
             .get("content-type")
-            .and_then(|mime| ImageMimeType::from_str(mime).ok())
+            .and_then(|mime| MimeType::from_str(mime).ok())
+            .filter(|mime| mime.is_image())
     }
-
+    fn get_audio_mime_type(&self) -> Option<MimeType> {
+        self.headers
+            .get("content-type")
+            .and_then(|mime| MimeType::from_str(mime).ok())
+            .filter(|mime| mime.is_audio())
+    }
     fn get_filename(&self) -> Option<String> {
         // TODO: DRY this out
         Headers::parse_params(
